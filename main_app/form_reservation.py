28b159ecdfe736cd3a5cadbd7b4c2aed8a133ff9
Khanh King <khanhnphan15@yahoo.com>
2023-08-23 01:42:22 -0400

    fix datepicker and layout

--- main_app/form_reservation.py
+++ main_app/form_reservation.py
@@ -5,4 +5,5 @@ from .models import Reservation
 class ReserveTableForm(forms.ModelForm):
     class Meta:
         model = Reservation
+        date = forms.DateField(widget=forms.DateInput(attrs={'class': 'datepicker'}))
         fields = '__all__'
